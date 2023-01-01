967d780c6c651b2d12ed36fd36604c6a542495e3
Aljawharah Alhammad <105801508+Aljawharahs@users.noreply.github.com>
2023-01-01 19:17:21 +0300

    Add files via upload
    First exercise

---
+++ e1.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var name: String ="Abdullah"
+    var f: Float = 20.6F
+    var d: Double= 8.9746325
+    var b:Byte = 25
+    var s: Short= 2022
+    var l: Long= 321984526954
+    var o: Boolean= true
+    var c: Char ='a'
+}
