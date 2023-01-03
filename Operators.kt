3b21ec4f846cece91898a16e205a745db3eb01e8
Aljawharah Alhammad <105801508+Aljawharahs@users.noreply.github.com>
2023-01-03 12:53:14 +0300

    Add files via upload

---
+++ Operators.kt
@@ -0,0 +1,14 @@
+fun main() {
+/*
+- false && false  soulution = False
+- 10%3            soulution = 1
+- 5>=5            soulution = True
+- false||true     soulution = True
+- 5-3* 2          soulution = -1
+- 3!=2            soulution = True
+- (6+6)/3         soulution = 4
+
+*/
+
+
+}
